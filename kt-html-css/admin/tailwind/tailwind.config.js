61aa4c9d959b06373089d52ddad7a3cb2e938822
TCL47-BITBUCKET <truongcongly.clt@gmail.com>
2024-08-23 23:52:21 +0700

    no message

--- kt-html-css/admin/tailwind/tailwind.config.js
+++ kt-html-css/admin/tailwind/tailwind.config.js
@@ -4,5 +4,13 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    ({ addUtilities }) => {
+      addUtilities({
+        '.ring-icon1-circle-fill': {
+          animation: 'ring-icon1-circle-fill 1s ease-in-out infinite',
+        },
+      });
+    },
+  ],
 }
