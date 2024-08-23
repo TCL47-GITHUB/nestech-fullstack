65ee5d9b4452059ba2b4d64509c2bcc391ff1353
TCL47-BITBUCKET <truongcongly.clt@gmail.com>
2024-08-23 07:31:31 +0700

    no message

--- kt-html-css/admin/sass/src/js/main.js
+++ kt-html-css/admin/sass/src/js/main.js
@@ -36,11 +36,24 @@ function toggleContent(buttonId, contentId) {
   }
 }
 
-function toggleShowHide(toggleID) {
+function toggleShowHideByID(event, toggleID, display) {
   var content = document.getElementById(toggleID);
+  var icon = event.currentTarget;
   var computedStyle = window.getComputedStyle(content);
+
   if (computedStyle.display === "none") {
-    content.style.display = "block";
+    content.style.display = display;
+
+    // Thêm sự kiện click vào document để ẩn khi click ra ngoài
+    document.addEventListener('click', function outsideClickListener(event) {
+      // Kiểm tra nếu click xảy ra bên ngoài cả thẻ <i> và phần tử content
+      if (!content.contains(event.target) && !icon.contains(event.target)) {
+        content.style.display = "none";
+
+        // Loại bỏ sự kiện này sau khi thực hiện để tránh lắng nghe liên tục
+        document.removeEventListener('click', outsideClickListener);
+      }
+    });
   } else {
     content.style.display = "none";
   }
@@ -74,4 +87,28 @@ function showBlock(buttonId) {
 function hide(buttonId){
   let button = document.getElementById(buttonId);
   button.style.display = 'none' ;
+}
+
+
+function btnChat() {
+  let btn = document.getElementsByClassName("btn-support-chat")[0];
+  let chat = document.getElementsByClassName("support-chat")[0];
+  btn.style.padding = '13px';
+  let children = btn.children;
+   // Kiểm tra trạng thái hiển thị của phần tử đầu tiên
+   if (children[0].style.display === 'block') {
+      // Nếu phần tử đầu tiên đang hiển thị, ẩn nó đi và hiển thị các phần tử khác
+      chat.style.transform = 'scale(0)';
+      children[0].style.display = 'none';
+      children[1].style.display = 'none';
+      children[2].style.display = 'block';
+      children[3].style.display = 'block';
+  } else {
+      // Nếu phần tử đầu tiên đang ẩn, hiển thị nó và ẩn các phần tử khác
+      chat.style.transform = 'scale(1)';
+      children[0].style.display = 'block';
+      children[1].style.display = 'none';
+      children[2].style.display = 'none';
+      children[3].style.display = 'none';
+  }
 }
